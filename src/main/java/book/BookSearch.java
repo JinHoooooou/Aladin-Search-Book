4cc9d8280bd9e8374804007c1a9fbc66575d5a5e
JinHoooooou <jinho4744@naver.com>
2020-04-25 15:36:58 +0900

    add booklist Json key for passing test

--- src/main/java/book/BookSearch.java
+++ src/main/java/book/BookSearch.java
@@ -1,5 +1,6 @@
 package book;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.nodes.Document;
@@ -10,16 +11,27 @@ public class BookSearch {
   public JSONObject searchBook(Document crawlingResult) throws JSONException {
     Elements elements = crawlingResult.select("div#Search3_Result");
     if (elements.size() == 0) {
-      return getJsonFormat(HttpStatusCode.NOT_FOUND.statusCode, HttpStatusCode.NOT_FOUND.message);
+      JSONArray bookList = new JSONArray();
+      return getJsonFormat(HttpStatusCode.NOT_FOUND.statusCode, HttpStatusCode.NOT_FOUND.message,
+          bookList);
     }
-    return getJsonFormat(HttpStatusCode.OK.statusCode, HttpStatusCode.OK.message);
+
+    JSONArray bookList = new JSONArray();
+//    Elements books = elements.select("b.bo3");
+//    for (int i = 0; i < books.size(); i++) {
+//      bookList.put(books.get(i).text());
+//    }
+    return getJsonFormat(HttpStatusCode.OK.statusCode, HttpStatusCode.OK.message, bookList);
   }
 
-  private JSONObject getJsonFormat(int statusCode, String message)
+  private JSONObject getJsonFormat(int statusCode, String message, JSONArray bookList)
       throws JSONException {
     JSONObject jsonObject = new JSONObject();
     jsonObject.put("status", statusCode);
     jsonObject.put("message", message);
+    jsonObject.put("bookList", bookList);
     return jsonObject;
   }
+
+
 }
