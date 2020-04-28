9bf5d219f6b53ddc0ba19fa0dc9a8c54464d06c1
JinHoooooou <jinho4744@naver.com>
2020-04-28 16:44:58 +0900

    add bookList for passing test

--- src/main/java/book/BookSearch.java
+++ src/main/java/book/BookSearch.java
@@ -1,5 +1,6 @@
 package book;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.nodes.Document;
@@ -9,17 +10,20 @@ public class BookSearch {
 
   public JSONObject searchBook(Document crawlingResult) throws JSONException {
     Elements elements = crawlingResult.select("div#Search3_Result");
+    JSONArray bookList = new JSONArray();
     if (elements.size() == 0) {
-      return getJsonFormat(HttpStatusCode.NOT_FOUND.statusCode, HttpStatusCode.NOT_FOUND.message);
+      return getJsonFormat(HttpStatusCode.NOT_FOUND.statusCode, HttpStatusCode.NOT_FOUND.message,
+          bookList);
     }
-    return getJsonFormat(HttpStatusCode.OK.statusCode, HttpStatusCode.OK.message);
+    return getJsonFormat(HttpStatusCode.OK.statusCode, HttpStatusCode.OK.message, bookList);
   }
 
-  private JSONObject getJsonFormat(int statusCode, String message)
+  private JSONObject getJsonFormat(int statusCode, String message, JSONArray bookList)
       throws JSONException {
     JSONObject jsonObject = new JSONObject();
     jsonObject.put("status", statusCode);
     jsonObject.put("message", message);
+    jsonObject.put("bookList", bookList);
     return jsonObject;
   }
 }
